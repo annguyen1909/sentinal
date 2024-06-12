6ffd563ed09ba031a520c13dd0cc5acb4d1d500a
s3777091 <s3777091@rmit.edu.vn>
2024-06-12 15:23:26 +0700

    docker/datbase

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,26 +1,47 @@
-import { Inter } from "next/font/google";
+import React from "react";
 import type { Metadata } from "next";
-import "../globals.css";
+import { Inter } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/themes";
+
+import "../globals.css";
+import LeftSidebar from "@/components/shared/LeftSidebar";
+import Bottombar from "@/components/shared/Bottombar";
+import RightSidebar from "@/components/shared/RightSidebar";
+import Topbar from "@/components/shared/Topbar";
+
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "CyberSentinal",
-  description: "A next.js 15 Meta CyberSentinal Application",
+  title: "Threads",
+  description: "A Next.js 13 Meta Threads application",
 };
 
-const inter = Inter({
-  subsets: ["latin"],
-});
-
 export default function RootLayout({
-  children
+  children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        baseTheme: dark,
+      }}
+    >
       <html lang="en">
-        <body className={inter.className}>{children}</body>
+        <body className={inter.className}>
+          <Topbar />
+
+          <main className="flex flex-row">
+            <LeftSidebar />
+            <section className="main-container">
+              <div className="w-full max-w-4xl">{children}</div>
+            </section>
+            {/* @ts-ignore */}
+            <RightSidebar />
+          </main>
+          <Bottombar />
+        </body>
       </html>
     </ClerkProvider>
   );
